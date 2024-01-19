1e7fde87b2820d0da8bf427e97108d8b1a5a0ba1
azouaoui <alexandre.zouaoui@inria.fr>
2024-01-19 16:48:27 +0100

    (src): update

--- src/model/base.py
+++ src/model/base.py
@@ -5,7 +5,8 @@ import torch
 
 class UnmixingModel:
     def __init__(self):
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        print(f"Using device: {self.device}")
 
     def __repr__(self):
         msg = f"{self.__class__.__name__}"
--- src/semisupervised.py
+++ src/semisupervised.py
@@ -8,7 +8,6 @@ import numpy as np
 
 from src.data.utils import SVD_projection
 from src.utils.metrics import SRE, aRMSE, compute_metric
-from src.utils.aligners import AbundancesAligner
 from src.data import Estimate
 
 log = logging.getLogger(__name__)
@@ -29,6 +28,9 @@ def main(ctx: mlxp.Context) -> None:
     Y, r, D = hsi.get_data()
     # Get image dimensions
     h, w = hsi.get_img_shape()
+    # Clip data
+    #Y[Y <= 0] = 0
+    #Y[Y >= 1] = 1
     # Apply noise
     Y = noise.apply(Y)
     # L2 normalization
@@ -40,24 +42,19 @@ def main(ctx: mlxp.Context) -> None:
     # Build model
     model = _instance_from_config(cfg.model)
     # Solve unmixing
-    A_hat = model.compute_abundances(Y, D, r=r, h=h, w=w)
+    A_hat, B_hat = model.compute_abundances(Y, D, r=r, h=h, w=w)
 
-    E_hat = np.zeros((Y.shape[0], r))
+    A_full = B_hat @ A_hat
+    E_hat = D @ B_hat
 
     logger.log_artifact(Estimate(E_hat, A_hat, h, w), "estimates")
 
     if hsi.has_GT():
         # Get ground truth
         _, A_gt = hsi.get_GT()
-        # NOTE: Alignment not needed
-        # Select only the first relevant components
-        # NOTE: Fix this code by using a custom index tied to the dataset
-        if cfg.force_align:
-            aligner = AbundancesAligner(Aref=A_gt)
-            A1 = aligner.fit_transform(A_hat)
-        else:
-            index = hsi.get_index()
-            A1 = A_hat[index]
+        # NOTE: Select only the first relevant components by using the dataset index
+        index = hsi.get_index()
+        A1 = A_full[index]
         # Get labels
         labels = hsi.get_labels()
         # Compute and log metrics
